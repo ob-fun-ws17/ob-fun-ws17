aedfa38e0ecd4f48a077f0a3211e4e4a6694004a
Oliver Braun <ob@cs.hm.edu>
2017-12-05 15:58:18 +0100

    aktueller Stand

---
+++ Main.hs
@@ -0,0 +1,63 @@
+module Main where
+
+
+main :: IO ()
+main = putStrLn "Hello World!"
+
+-- tupFunc :: (Int, String) -> (Int, String) -> (Int, String)
+-- tupFunc x y = (fst x + fst y, snd x ++ snd y)
+
+-- tupFunc' (a,b) (c,d) = (a + c, b  ++ d)
+
+add :: Num a => a -> a -> a
+add x y = x + y
+addFive :: Num a => a -> a
+addFive = add 5
+
+addUnCurried :: Num a => (a, a) -> a
+addUnCurried (x,y) = x + y
+
+data Trivial = Trivial
+
+instance Show Trivial where
+  show Trivial = "Trivial"
+
+instance Eq Trivial where
+  Trivial == Trivial = True
+
+data DayOfWeek = Mon | Tue | Wed | Thu | Fri | Sat | Sun
+  deriving (Eq, Show, Ord, Enum)
+
+
+f' :: ((Int, Int), Bool) -> ((Int, Int), Bool)
+f' (t@(5, 3), b)     = (t, b)
+f' ((x, y), True)  = ((y, x), False)
+f' (t, False) = (t, True)
+
+myAbs :: Integer -> Integer
+-- myAbs x = if x < 0 then -x else x
+
+myAbs x | x < 0 = -x
+        -- | x == 0 = 0
+        | otherwise = x
+
+factorial :: Integer -> Maybe Integer
+factorial 0 = Just 1
+factorial n
+  | n > 0 = case factorial (n-1) of
+      Nothing -> Nothing
+      Just nMinus1 -> Just $ n * nMinus1
+  | otherwise = Nothing
+
+-- otherwise = True
+
+-- st' (x,_) = x
+
+-- fst3 (x,_,_) = x
+
+-- fst4 (x,_,_,_) = x
+
+data Identity a = Identity a
+
+instance Eq a => Eq (Identity a) where
+  (==) (Identity v) (Identity v') = v == v'
